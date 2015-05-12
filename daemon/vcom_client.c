2d4e6357167ef692c298ccde8e4d14ea3e739627
samuel.cheng@advantech.com.tw <samuel.cheng@advantech.com.tw@9d855a72-824a-9447-89d5-058a706077a7>
2015-05-12 05:05:13 +0000

    remove all port_ops
    git-svn-id: https://172.20.2.44/svn/iagicom/driver/icom_pseudo_tty_driver/branch/mmap@4119 9d855a72-824a-9447-89d5-058a706077a7

--- daemon/vcom_client.c
+++ daemon/vcom_client.c
@@ -21,7 +21,7 @@
 #define RBUF_SIZE	4096
 //static int log_fd = -1;
 extern void * stk_mon; 
-struct vc_ops * vc_recv_desp(struct vc_attr *port, struct vc_ops * port_ops)
+struct vc_ops * vc_recv_desp(struct vc_attr *port)
 {
 	int len;
 	int hdr_len;
@@ -64,7 +64,7 @@ struct vc_ops * vc_recv_desp(struct vc_attr *port, struct vc_ops * port_ops)
 			return stk_curnt(stk)->err(port, "VCOM len miss-match", len);
 	}
 
-	return try_ops3(port_ops, recv, port, buf, hdr_len + packet_len);
+	return try_ops3(stk_curnt(stk), recv, port, buf, hdr_len + packet_len);
 }
 #undef RBUF_SIZE
 /*
@@ -144,7 +144,6 @@ int startup(int argc, char **argv, struct vc_attr *port)
 int main(int argc, char **argv)
 {
 	struct vc_attr port;
-	struct vc_ops * port_ops;
 	struct timeval tv;
 	fd_set rfds;
 	fd_set efds;
@@ -154,9 +153,12 @@ int main(int argc, char **argv)
 	unsigned int intflags;
 	unsigned int lrecv;
 	char filename[64];
+	struct stk_vc * stk;
 
 	const unsigned int psec = VC_PULL_PSEC;
 	const unsigned int pusec = VC_PULL_PUSEC;
+
+	stk = &port.stk;
 	
 	port.ip_red = 0;
 	stk_mon = 0;	// stack monitor init
@@ -177,13 +179,13 @@ int main(int argc, char **argv)
 		printf("failed to mmap\n");
 		return 0;
 	}
-	port.stk.top = -1;	// state mechine stack init
+	stk->top = -1;	// state mechine stack init
 	port.sk	= -1;
 	vc_buf_setup(&port, VC_BUF_TX);
 	vc_buf_setup(&port, VC_BUF_RX);
 	vc_buf_setup(&port, VC_BUF_ATTR);
-	stk_push(&port.stk, &vc_netdown_ops);
-	port_ops = stk_curnt(&port.stk)->init(&port);
+	stk_push(stk, &vc_netdown_ops);
+	stk_curnt(stk)->init(&port);
 	lrecv = 0;
 
 	while(1){
@@ -197,9 +199,9 @@ int main(int argc, char **argv)
 
 		if(check_attr_stat(&port, is_open) == ATTR_DIFF){
 			if(attr_p(&port, is_open)){
-				port_ops = port_ops->open(&port);
+				stk_curnt(stk)->open(&port);
 			}else{
-				port_ops = port_ops->close(&port);
+				stk_curnt(stk)->close(&port);
 			}
 		}
 
@@ -209,24 +211,24 @@ int main(int argc, char **argv)
 		check_attr_stat(&port, byte) == ATTR_DIFF||
 		check_attr_stat(&port, stop) == ATTR_DIFF||
 		check_attr_stat(&port, ms) == ATTR_DIFF){
-			port_ops = try_ops(port_ops, ioctl, &port);
+			try_ops(stk_curnt(stk), ioctl, &port);
 		}
 		
 		if(port.xmit_pending == 0){
 			if(!is_rb_empty(port.rx)){
-				port_ops = try_ops(port_ops, xmit, &port);
+				try_ops(stk_curnt(stk), xmit, &port);
 			}
 		}
 
 		if(!is_rb_empty(port.tx)){
-			port_ops = try_ops(port_ops, pause, &port);
+			try_ops(stk_curnt(stk), pause, &port);
 		}
 
 
 		FD_SET(port.fd, &efds);
 		FD_SET(port.sk, &rfds);
 
-		try_ops5(port_ops, event, &port, &tv, &rfds, &wfds, &efds);
+		try_ops5(stk_curnt(stk), event, &port, &tv, &rfds, &wfds, &efds);
 
 		intflags = ADV_INT_RX|ADV_INT_TX;
 
@@ -243,7 +245,7 @@ int main(int argc, char **argv)
 	
 		ret = select(maxfd + 1, &rfds, &wfds, &efds, &tv);
 		if(ret == 0 ){
-			port_ops = try_ops(port_ops, poll, &port);
+			try_ops(stk_curnt(stk), poll, &port);
 			continue;
 		}
 
@@ -253,12 +255,12 @@ int main(int argc, char **argv)
 
 		if(FD_ISSET(port.sk, &rfds)){
 			lrecv = 0;
-			port_ops = vc_recv_desp(&port, port_ops);
+			vc_recv_desp(&port);
 		}else if(port.sk >= 0){
 			unsigned int used = VC_TIME_USED(tv);
 			lrecv += (used > 0)?used:1;
 			if(lrecv > VC_PULL_TIME){
-				port_ops = port_ops->err(&port, "PROTO timeout", 0);
+				stk_curnt(stk)->err(&port, "PROTO timeout", 0);
 				printf("lrecv = %u\n", lrecv);
 				lrecv = 0;
 			}
@@ -270,7 +272,7 @@ int main(int argc, char **argv)
 
 		if(FD_ISSET(port.fd, &wfds)){
 			vc_buf_update(&port, VC_BUF_TX);
-			port_ops = try_ops(port_ops, resume, &port);
+			try_ops(stk_curnt(stk), resume, &port);
 		}
 	}
 
