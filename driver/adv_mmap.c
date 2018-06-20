5975fc14abfe101b73583ac7ae19b6785ae44d3f
samuel.cheng@advantech.com.tw <samuel.cheng@advantech.com.tw@9d855a72-824a-9447-89d5-058a706077a7>
2018-06-20 07:40:20 +0000

    support linux > 4.10.0
    git-svn-id: https://172.20.2.44/svn/iagicom/driver/icom_pseudo_tty_driver/branch/mmap@5430 9d855a72-824a-9447-89d5-058a706077a7

--- driver/adv_mmap.c
+++ driver/adv_mmap.c
@@ -4,6 +4,7 @@
 #include <linux/errno.h>        /* error codes */
 #include <linux/fs.h>
 #include <asm/pgtable.h>
+#include <linux/version.h>
 #include "advvcom.h"
 
 void adv_vma_open(struct vm_area_struct *vma)
@@ -16,10 +17,12 @@ void adv_vma_close(struct vm_area_struct *vma)
 //	printk("%s(%d)\n", __func__, __LINE__);
 }
 
-int adv_vma_nopage(struct vm_area_struct *vma, struct vm_fault *vmf)
+#if  LINUX_VERSION_CODE >= KERNEL_VERSION(4,10,0)
+int adv_vma_nopage(struct vm_fault *vmf)
 {
 	unsigned long offset;
 	struct adv_vcom * data;
+	struct vm_area_struct *vma = vmf->vma;
 	struct page *page = NULL;
 //	void * pageptr = NULL;
 	int ret = 0;
@@ -28,7 +31,7 @@ int adv_vma_nopage(struct vm_area_struct *vma, struct vm_fault *vmf)
 	data = vma->vm_private_data;
 
 //	printk("data = %x\n", data);
-	offset = (unsigned long)(vmf->virtual_address - vma->vm_start) + (vma->vm_pgoff << PAGE_SHIFT);
+	offset = (unsigned long)(vmf->address - vma->vm_start) + (vma->vm_pgoff << PAGE_SHIFT);
 //	printk("offset = %u \n", offset);
 //	printk("totalsize = %d\n", (data->rx.size + data->tx.size));
 
@@ -59,7 +62,41 @@ int adv_vma_nopage(struct vm_area_struct *vma, struct vm_fault *vmf)
 	out:
 	return ret;
 }
+#else
+int adv_vma_nopage(struct vm_area_struct *vma, struct vm_fault *vmf)
+{
+	unsigned long offset;
+	struct adv_vcom * data;
+	struct page *page = NULL;
+	int ret = 0;
 
+	data = vma->vm_private_data;
+
+	offset = (unsigned long)(vmf->virtual_address - vma->vm_start) + (vma->vm_pgoff << PAGE_SHIFT);
+
+
+	if(offset > (data->rx.size + data->tx.size) ){
+		printk("%s(%d)\n", __func__, __LINE__);
+		goto out;
+	}
+	
+	if(offset < (data->tx.size + data->tx.begin)){
+		page = virt_to_page(data->tx.data);
+	}else if(offset < (data->rx.size + data->rx.begin)){
+		page = virt_to_page(data->rx.data);
+	}else if(offset < (data->attr.size + data->attr.begin)){
+		page = virt_to_page(data->attr.data);
+	}else{
+		goto out;
+	}
+
+	get_page(page);
+	vmf->page = page;
+		
+	out:
+	return ret;
+}
+#endif
 
 struct vm_operations_struct adv_vm_ops = {
         .open =     adv_vma_open,
