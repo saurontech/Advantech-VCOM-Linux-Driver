01923bc0d766db60021039bf8fccf5eb44eb8d8b
samuel.cheng@advantech.com.tw <samuel.cheng@advantech.com.tw@9d855a72-824a-9447-89d5-058a706077a7>
2021-07-06 02:31:31 +0000

    add option "systemd" to Config.mk
    git-svn-id: https://172.20.2.44/svn/iagicom/driver/icom_pseudo_tty_driver/branch/mmap@6274 9d855a72-824a-9447-89d5-058a706077a7

--- Config.mk
+++ Config.mk
@@ -2,11 +2,19 @@ INSTALL_PATH = /usr/local/advtty/
 MODNAME = advvcom
 VERSION = 1
 
+#build VCOM with TLS support (y/n)
 TLS = y
+#install VCOM with DKMS (y/n)
 DKMS = y
+#install register VCOM to systemd (y/n)
+SYSTEMD = n
 
+#DSA length for the rootCA.key(private key)
 CA_DSA_LEN = 2048
+#life-time of the rootCA.pem public key
 CA_DAYS = 1024
 
+#DSA length for the vcom.pem public key
 CERT_DSA_LEN = 2048
+#life-time for the vcom.pem public key
 CERT_DAYS = 1024
