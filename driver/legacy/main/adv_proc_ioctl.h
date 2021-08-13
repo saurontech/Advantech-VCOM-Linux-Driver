0aeb6f69184333c0789ab9684fcb19dad87e7133
samuel.cheng@advantech.com.tw <samuel.cheng@advantech.com.tw@9d855a72-824a-9447-89d5-058a706077a7>
2021-08-13 09:38:30 +0000

    first workable commit on centOS 8
    git-svn-id: https://172.20.2.44/svn/iagicom/driver/icom_pseudo_tty_driver/branch/mmap@6353 9d855a72-824a-9447-89d5-058a706077a7

--- driver/legacy/main/adv_proc_ioctl.h
+++ driver/legacy/main/adv_proc_ioctl.h
@@ -2,13 +2,7 @@
 #define _ADV_PROC_IOCTL_H
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0)
 //@ current
-#elif defined(RHEL_RELEASE_CODE)
-#	if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(8,1)
-//	@current
-	#else
-		long adv_proc_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
-#	endif
 #else//oldest
-		long adv_proc_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+long adv_proc_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
 #endif
 #endif
