92292243b5f8fa0c2f0bf3e588505564525c87d9
root <root@localhost.localdomain>
2022-12-15 18:32:55 +0800

    first fix for RHEL91

---
+++ driver/legacy/main/adv_proc_open.h
@@ -0,0 +1,36 @@
+#ifndef ADV_PROC_OPEN_H
+#define ADV_PROC_OPEN_H
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0)
+
+#else
+int adv_proc_open(struct inode *inode, struct file *filp)
+{
+	struct adv_vcom * data;
+#if defined(RHEL_RELEASE_CODE)
+#	if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9,1)
+	data = pde_data(inode);
+#	else
+	data = PDE_DATA(inode);
+#	endif
+#endif
+
+	filp->private_data = data;
+	return 0;
+}
+
+int adv_proc_release(struct inode *inode, struct file *filp)
+{
+	struct adv_vcom * data;
+
+#if defined(RHEL_RELEASE_CODE)
+#	if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9,1)
+	data = pde_data(inode);
+#	else
+	data = PDE_DATA(inode);
+#	endif
+#endif
+
+	return 0;
+}
+#endif
+#endif
