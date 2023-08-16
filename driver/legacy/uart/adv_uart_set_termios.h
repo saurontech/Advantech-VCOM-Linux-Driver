1d1dab73762e9a985f0f36d0b7ca1eceace02960
samuel cheng <samuel.cheng@advantech.com.tw>
2023-08-16 13:31:57 +0800

    fix build for RHEL 9.2

--- driver/legacy/uart/adv_uart_set_termios.h
+++ driver/legacy/uart/adv_uart_set_termios.h
@@ -3,8 +3,19 @@
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6,1,0)
 //@ current
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,0,0)
+#	if defined(RHEL_RELEASE_CODE)
+#		if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9,2)
+#			define _BACK_PORT_6_1_0
+#		endif
+#	endif
+#	ifdef _BACK_PORT_6_1_0
+void adv_uart_set_termios(struct uart_port *port, struct ktermios *termios,
+				const struct ktermios *old);
+#	else
 void adv_uart_set_termios(struct uart_port *port, struct ktermios *termios,
 		       struct ktermios *old);
+#	endif
+#	undef _BACK_PORT_6_1_0
 #else
 void adv_uart_set_termios(struct uart_port *port, struct ktermios *termios,
 		       struct ktermios *old);
